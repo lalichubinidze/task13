bd8c0fad21c6288639646905e389ab103cd53e52
lali <lalichubinidze.1999@gmail.com>
2022-07-05 00:18:22 +0400

    Initial Commit

---
+++ Task 13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 13
+//
+//  Created by iveri gamezardashvili on 7/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
